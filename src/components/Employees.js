abb820b95b5b64f63b888fee3cb4b28adfb5ba04
JeevarehaR <jeev.reh@gmail.com>
2022-12-25 14:57:35 +0530

    CRUD added

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Jeeva reha",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Bhava Dharani",
+    Age: "24",
+  },
+  {
+    id: "3",
+    Name: "Subhasri Lakshmi",
+    Age: "25",
+  },
+];
+
+export default Employees;
